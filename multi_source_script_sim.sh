ff59363600ae877c2bf189fb1c4dffd7b1f33c2a
Nils9231 <nils.timmermann@tuhh.de>
2018-09-15 13:31:04 +0200

    adjustment to new launch system

--- multi_source_script_sim.sh
+++ multi_source_script_sim.sh
@@ -1,10 +1,8 @@
-	
-	
-	make posix_sitl_default
-	source Tools/setup_gazebo.bash $(pwd) $(pwd)/build_posix_sitl_default
-	export PX4_HOME_LAT=0
-	export PX4_HOME_LON=0
-	export PX4_HOME_ALT=0
-    	export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
-    	export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
-    	roslaunch mav_gazebo multi_simulation.launch
+make posix_sitl_default
+source Tools/setup_gazebo.bash $(pwd) $(pwd)/build_posix_sitl_default
+export PX4_HOME_LAT=0
+export PX4_HOME_LON=0
+export PX4_HOME_ALT=0
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
+roslaunch px4 multi_hippocampus.launch
--- single_source_script_sim.sh
+++ single_source_script_sim.sh
@@ -2,12 +2,14 @@
 #	export PX4_HOME_LON=0
 #	export PX4_HOME_ALT=0
 
-	export PX4_HOME_LAT=28.452386
-	export PX4_HOME_LON=-13.867138
-	export PX4_HOME_ALT=28.5
-	
-	make posix_sitl_default 
-	source Tools/setup_gazebo.bash $(pwd) $(pwd)/build_posix_sitl_default
-	export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
-    	export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
-    	roslaunch mav_gazebo single_simulation.launch
+make posix_sitl_default 
+source Tools/setup_gazebo.bash $(pwd) $(pwd)/build_posix_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
+
+export PX4_HOME_LAT=28.452386
+export PX4_HOME_LON=-13.867138
+export PX4_HOME_ALT=28.5
+
+roslaunch px4 single_hippocampus.launch
+
