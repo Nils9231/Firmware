caef505bfbd69d6f3339b75948400d8a821f53ef
Daniel Duecker <daniel.duecker@tuhh.de>
2018-09-17 18:02:42 +0200

    minor fixes

--- start_mavros_sitl.sh
+++ start_mavros_sitl.sh
@@ -1,5 +1,5 @@
 make posix_sitl_default #gazebo
-#source ~/catkin_ws/devel/setup.bash    // (optional)
+source ~/catkin_ws/devel/setup.bash    # (optional)
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/posix_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
@@ -8,4 +8,4 @@ export PX4_HOME_LAT=28.452386
 export PX4_HOME_LON=-13.867138
 export PX4_HOME_ALT=28.5
 
-roslaunch px4 mavros_posix_sitl.launch
+HEADLESS=1 roslaunch px4 mavros_posix_sitl.launch
